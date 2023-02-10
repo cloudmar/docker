13714d650780c803aaaa168c367fe9cd93ab14c2
cloudmar <124941397+cloudmar@users.noreply.github.com>
2023-02-10 01:36:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,23 @@
-sudo apt install docker.io
-sudo apt install docker-compose
+# Docker install
+
+echo "Docker install"
+
+sudo apt update
+
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Docker compose install
+
+echo "Docker compose install"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
