775a7a5463c5513e4cd873230e4ec216e7d41c3b
cloudmar <124941397+cloudmar@users.noreply.github.com>
2023-02-10 01:43:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,3 +21,7 @@ echo "Docker compose install"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker --version
+
+sudo docker-compose --version
