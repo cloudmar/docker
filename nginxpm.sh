19ba2978f25359a0da992902fa21208296591c62
cloudmar <124941397+cloudmar@users.noreply.github.com>
2023-02-10 18:56:10 +0100

    Update nginxpm.sh

--- nginxpm.sh
+++ nginxpm.sh
@@ -51,7 +51,7 @@ services:
       MYSQL_USER: '$user'
       MYSQL_PASSWORD: '$password'
     volumes:
-      - ./data/mysql: /var/lib/mysql
+      - ./data/mysql:/var/lib/mysql
 EOF
 
 sudo docker-compose up -d
